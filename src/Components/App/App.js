5821924b6490de49a06088c935f02e3830d33301
kylian <kylian.mendy@hetic.net>
2020-11-20 01:49:19 +0100

    header/footer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import Accueil from '../Accueil/Accueil';
 import SearchBar from '../SearchBar/SearchBar';
-
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
 function App() {
   return (
     <div>
+      <Header />
       <SearchBar />
       <Accueil /> 
+      <Footer />
     </div>    
   );
 }
